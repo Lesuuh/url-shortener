2f6c96ad390f3b18cdf913df72ae3fcb63e6f63d
Lesuuh <lesuuh01@gmail.com>
2026-06-17 21:53:46 +0100

    Add token verification middleware for user authentication

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+async function verifyToken(req: Request, res: Response, next: NextFunction) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: "Access denied. No session token provided." });
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET || "fall-back string";
+    const decoded = jwt.verify(token, secret) as any;
+
+    (req as any).user_id = decoded.userId;
+
+    return next();
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid or expired session token." });
+  }
+}
